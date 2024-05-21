cca3b330500050233d28b748d734e6418ac763fb
Michal Sroczynski <michal@digitalwarehou.se>
2024-05-21 12:38:29 +0200

    Tests with datepickers

--- tests/usePageObject.spec.ts
+++ tests/usePageObject.spec.ts
@@ -1,6 +1,8 @@
 import {test, expect} from '@playwright/test'
 import {NavigationPage} from '../page-objects/navigationPage'
 import {FormLayoutsPage} from '../page-objects/formLayoutsPage'
+import {DatepickerPage} from '../page-objects/datePickerPage'
+
 
 test.beforeEach(async({page}) => {
   await page.goto('http://localhost:4200')
@@ -16,11 +18,20 @@ test('navigate through pages', async({page}) => {
   await navigateTo.tooltipPage()
 })
 
-test('use parametrized methods', async({page}) => {
+test('use parametrized login methods', async({page}) => {
   const navigateTo = new NavigationPage(page)
   const onFormsLayoutPage = new FormLayoutsPage(page)
   
   await navigateTo.formLayoutsPage()
   await onFormsLayoutPage.submitUsingTheGridFormWithCredentialsAndSelectOption("test@test.com", "test1234", 'Option 1' )
   await onFormsLayoutPage.submitInlineFormWithNameEmailAndCheckbox("John Smith", "John@test.com", true)
+})
+
+test('use parametrized datepicker methods', async({page}) => {
+  const navigateTo = new NavigationPage(page)
+  const onDatepickerPage = new DatepickerPage(page)
+
+  await navigateTo.datepickerPage()
+  await onDatepickerPage.selectCommonDatePickerDateFromToday(7)
+  await onDatepickerPage.selectDatepickerWithRangeFromToday(2, 5)
 })
